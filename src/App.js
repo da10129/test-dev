3908507cad6ee03d457eaca5a332f38a20e61af5
da10129 <119820010+da10129@users.noreply.github.com>
2022-12-06 05:40:07 +0900

    installAuth-7

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import "@aws-amplify/ui-react/styles.css";
-// import awsExports from './aws-exports';
+import awsExports from './aws-exports';
 import { isEmpty } from './util';
+Amplify.configure(awsExports);
 
 function App({signOut,user}) {
   return (
